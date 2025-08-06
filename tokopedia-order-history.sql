618c7dae77e1a24e66e749eb39baff7a98f6feeb
Juna <itujun7@gmail.com>
2025-08-06 19:18:04 +0700

    Insert data dummy

--- tokopedia-order-history.sql
+++ tokopedia-order-history.sql
@@ -44,3 +44,21 @@ CREATE TABLE OrderDetails(
 
 SHOW TABLES;
 DESC OrderDetails;
+
+
+INSERT INTO orders(id, created_at, seller_id, seller_name, buyer_id, buyer_name, shipping_name, shipping_address, shipping_phone, logistic_id, logistic_name, payment_method_id, payment_method_name, total_quantity, total_weight, total_product_amount, total_shipping_cost, total_shopping_amount, service_charge, total_amount) 
+VALUES ('122', NOW(), 'galery-olahraga-indonesia', 'Galery Olahraga Indonesia', 'jokomorro', 'Joko Morro', 'Budi Nugraha', 'Jl. Raya Xxx, Jakarta, DKI Jakarta, 434343', '081234567890', 'sicepat', 'Sicepat Gokil', 'debit online', 'Debit Online', 5, 5360, 508500, 60000, 568500, 1000, 569500);
+
+SELECT * FROM orders;
+
+
+INSERT INTO orderdetails(id, order_id, product_id, product_name, product_weight, product_price, quantity, total_amount)
+VALUES ('1', '122', 'p1', 'Basketball', 2300, 177900, 2, 355800);
+INSERT INTO orderdetails(id, order_id, product_id, product_name, product_weight, product_price, quantity, total_amount)
+VALUES ('2', '122', 'p2', 'Basketball Warna', 500, 98900, 1, 98900);
+INSERT INTO orderdetails(id, order_id, product_id, product_name, product_weight, product_price, quantity, total_amount)
+VALUES ('3', '122', 'p3', 'Pentil Pompa Bola', 54, 9900, 1, 9900);
+INSERT INTO orderdetails(id, order_id, product_id, product_name, product_weight, product_price, quantity, total_amount)
+VALUES ('4', '122', 'p4', 'Pompa Bola', 198, 43900, 1, 43900);
+
+SELECT * FROM orderdetails;
